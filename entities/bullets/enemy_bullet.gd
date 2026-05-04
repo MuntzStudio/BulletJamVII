8b54d39c501faf0708e336b024a61396d2604d0d
Sk Muntazir <muntazirsk143@gmail.com>
2026-05-05 00:38:35 +0530

    fixed hitbox

--- entities/bullets/enemy_bullet.gd
+++ entities/bullets/enemy_bullet.gd
@@ -7,8 +7,13 @@ var direction: Vector3 = Vector3.ZERO
 
 func _ready():
 	super._ready()
+	set_active(true)
+	body_entered.connect(_on_hit)
 	await get_tree().create_timer(lifetime).timeout
 	queue_free()
 
 func _process(delta):
 	position += direction * speed * delta
+
+func _on_hit(_body: Node3D) -> void:
+	queue_free()
