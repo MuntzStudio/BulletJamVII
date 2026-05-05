8cea90cf13610046487cb0a00b75777272cba34f
WITBoudicca <broddiccus@gmail.com>
2026-05-05 15:32:44 -0400

    Added Knife Boy
    added Knife Boy and scaled both of htem to fit the test level

---
+++ enemy_Eye.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+
+func _on_animation_player_animation_started(anim_name: StringName) -> void:
+	print(anim_name)
+	if anim_name in ["Hit", "Dodge", "Throw"]:
+		print(anim_name)
+		$Armature/Skeleton3D/Eyes.visible = false
+		$Armature/Skeleton3D/ClosedEyes.visible = true
+	else:
+		$Armature/Skeleton3D/Eyes.visible = true
+		$Armature/Skeleton3D/ClosedEyes.visible = false
+		
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name in ["Hit", "Dodge", "Throw"]:
+		$Armature/Skeleton3D/Eyes.visible = true
+		$Armature/Skeleton3D/ClosedEyes.visible = false
+	else:
+		$Armature/Skeleton3D/Eyes.visible = true
+		$Armature/Skeleton3D/ClosedEyes.visible = false
