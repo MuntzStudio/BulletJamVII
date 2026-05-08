a4aa801575a98d3faffd5498fc1c71cb35b327c0
WITBoudicca <broddiccus@gmail.com>
2026-05-08 18:07:54 -0400

    Fixed enemy eyes
    forgot to add boomerang boys eye thingy and i fixed the bow boys having their eyes closed forever after they fire once

--- scripts/enemy_Eye.gd
+++ scripts/enemy_Eye.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 func _on_animation_player_animation_started(anim_name: StringName) -> void:
 	print(anim_name)
-	if anim_name in ["Hit", "Dodge", "Throw", "Fire"]:
+	if anim_name in ["Hit", "Dodge", "Throw", "Fire", "Throw1", "Throw2"]:
 		print(anim_name)
 		$Armature/Skeleton3D/Eyes.visible = false
 		$Armature/Skeleton3D/ClosedEyes.visible = true
@@ -15,7 +15,7 @@ func _on_animation_player_animation_started(anim_name: StringName) -> void:
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name in ["Hit", "Dodge", "Throw"]:
+	if anim_name in ["Hit", "Dodge", "Throw", "Fire", "Throw1", "Throw2"]:
 		$Armature/Skeleton3D/Eyes.visible = true
 		$Armature/Skeleton3D/ClosedEyes.visible = false
 	else:
