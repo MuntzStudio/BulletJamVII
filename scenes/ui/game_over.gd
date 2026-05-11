322459f103833952ab3ef05639f7905aae263e83
Sk Muntazir <muntazirsk143@gmail.com>
2026-05-11 21:33:35 +0530

    reload in

--- scenes/ui/game_over.gd
+++ scenes/ui/game_over.gd
@@ -1,14 +1,15 @@
 extends Control
-
 const restart_scene = "res://scenes/ui/main_menu.tscn"
 @onready var restart: Button = %Restart
 @onready var quit: Button = %Quit
+@onready var reload: Button = %Reload
 @export var gameover_song : AudioStream
 
 func _ready() -> void:
 	Audio.play_ambience(gameover_song,-1.0)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	restart.pressed.connect(_on_restart_pressed)
+	reload.pressed.connect(_on_reload_pressed)
 	quit.pressed.connect(_on_quit_pressed)
 	
 	restart.focus_entered.connect(_on_restart_focus_entered)
@@ -25,6 +26,16 @@ func _on_restart_pressed() -> void:
 		LoadManager.load_scene(restart_scene)
 	else:
 		print("restart_scene is null!")
+
+func _on_reload_pressed() -> void:
+	Audio.ui_select()
+	get_tree().paused = false
+	if SaveManager.has_save():
+		SaveManager.load_game()
+		var saved_scene = SaveManager.get_value("current_scene", restart_scene)
+		LoadManager.load_scene(saved_scene)
+	else:
+		LoadManager.load_scene(restart_scene)
 
 func _on_quit_pressed() -> void:
 	Audio.ui_select()
